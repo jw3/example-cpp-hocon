658c97330d5ddb49d601a0c715f39904afe92e72
John Wass <wassj@ctc.com>
2017-12-17 16:01:31 -0500

    cleanup

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ using namespace hocon;
 
 int main() {
     auto conf = config::parse_file_any_syntax("../test.conf")->resolve();
-    cout << conf->root()->render() << endl;
+    cout << conf->root()->render(config_render_options(false, false, true, true)) << endl;
     return 0;
 }
